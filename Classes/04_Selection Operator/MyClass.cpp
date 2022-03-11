d81b00770a3684d7ac43991ee8b6b3918f55cb1f
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-03-11 22:57:04 +0530

    ud

---
+++ Classes/04_Selection Operator/MyClass.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "MyClass.h"
+using namespace std;
+
+MyClass::MyClass() {
+}
+
+void MyClass::myPrint() {
+    cout<<"Hello, World!\n";
+}
+    
---
+++ Classes/04_Selection Operator/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "MyClass.h"
+using namespace std;
+
+int main() {
+    MyClass obj;
+    obj.myPrint();
+    return 0;
+}
