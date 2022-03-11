0af2a234d59faa5482fef00a2fb17c5d35fed6e9
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-03-11 23:08:04 +0530

    ud

--- Classes/04_Selection Operator/main.cpp
+++ Classes/04_Selection Operator/main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main() {
     MyClass obj;
+    MyClass *ptr=&obj;  // Address is a
+    // Dot Operator (uses object name)
     obj.myPrint();
+    // Selection operator (uses a pointer)
+    ptr->myPrint();
     return 0;
 }
