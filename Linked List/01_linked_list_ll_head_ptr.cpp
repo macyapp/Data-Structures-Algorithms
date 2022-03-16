84f4939693180e0f22d75299863a6d5dc3620aca
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-03-16 22:26:15 +0530

    ud

---
+++ Linked List/01_linked_list_ll_head_ptr.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class node {
+public:
+    int data;
+    node *next;   
+};
+
+void printList(node *p) {
+    while(p!=NULL) {
+        cout<<p->data<<"->";
+        p=p->next;
+    }
+    cout<<"NULL\n";
+}
+
+int main() {
+    node *head=NULL;
+    node *ptr;
+    head=new node;
+    ptr=head;
+
+    ptr->data=10;
+    ptr->next=new node;
+    ptr=ptr->next;
+
+    ptr->data=20;
+    ptr->next=new node;
+    ptr=ptr->next;
+
+    ptr->data=30;
+    ptr->next=NULL;
+    
+    printList(head);
+
+    return 0;
+}
