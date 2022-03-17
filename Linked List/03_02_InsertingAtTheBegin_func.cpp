2db48ae27c572290c712129b10c8207548eaf13e
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-03-17 12:05:22 +0530

    ud

--- Linked List/03_02_InsertingAtTheBegin_func.cpp
+++ Linked List/03_02_InsertingAtTheBegin_func.cpp
@@ -11,40 +11,52 @@ public:
     }   
 };
 
-void printList(node *ptr) {
-    while(ptr!=NULL) {
-        cout<<ptr->data<<"->";
-        ptr=ptr->next;
-    }
-    if(ptr==NULL)
-        cout<<"NULL\n";
-}
-
-void insertBegin(node *&head, int x) {
-    node *ptr=new node(x);
-    ptr->next=head;
-    head=ptr;
-}
-
-int main() {
+class LinkedList {
+public:
     node *head;
     node *ptr;
-    // Initializing list
-    /*
-    ptr=new node(10);
-    head=ptr;
-    
-    ptr->next=new node(20);
-    ptr=ptr->next;
+    void printList() {
+        ptr=head;
+        while(ptr!=NULL) {
+            cout<<ptr->data<<"->";
+            ptr=ptr->next;
+        }
+        if(ptr==NULL)
+            cout<<"NULL\n";
+    }
 
-    ptr->next=new node(30);
-    ptr=ptr->next;
-    */
-   
-    printList(head);
-    // Inserting at the beginning
-    insertBegin(head,15);
-    
-    printList(head);
+    void insertBegin(int x) {
+        node *ptr=new node(x);
+        ptr->next=head;
+        head=ptr;
+    }
+
+    void insertEnd(int x) {
+        ptr=head;
+        if(ptr==NULL) {                 // If list is empty
+            ptr=new node(x);
+            head=ptr;
+        }
+        else {
+            while(ptr->next!=NULL) {    // If list has not empty
+                ptr=ptr->next;
+            }
+            ptr->next=new node(x);
+        }
+    }
+};
+
+int main() {
+    LinkedList ll;
+    ll.printList();
+    ll.insertEnd(15);
+    ll.printList();
+    ll.insertEnd(10);
+    ll.printList();
+    ll.insertEnd(20);
+    ll.printList();
+    ll.insertEnd(30);
+    ll.printList();
+
     return 0;
 }
