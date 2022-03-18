de0f1801444a35dce411c9d2b94f04d0588082dc
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-03-18 09:09:56 +0530

    ud

--- Linked List/main.cpp
+++ Linked List/main.cpp
@@ -11,33 +11,112 @@ public:
     }   
 };
 
-void printList(node *ptr) {
-    while(ptr!=NULL) {
-        cout<<ptr->data<<"->";
-        ptr=ptr->next;
-    }
-    if(ptr==NULL)
-        cout<<"NULL\n";
-}
-
-int main() {
+class LinkedList {
+public:
     node *head;
     node *ptr;
-    // Initializing list
-    ptr=new node(10);
-    head=ptr;
-    
-    ptr->next=new node(20);
-    ptr=ptr->next;
+    void printList() {
+        ptr=head;
+        while(ptr!=NULL) {
+            cout<<ptr->data<<"->";
+            ptr=ptr->next;
+        }
+        if(ptr==NULL)
+            cout<<"NULL\n";
+    }
 
-    ptr->next=new node(30);
-    ptr=ptr->next;
-
-    // Inserting at the beginning
-    ptr=new node(15);
-    ptr->next=head;
-    head=ptr;
+    void insertBegin(int x) {
+        ptr=new node(x);
+        ptr->next=head;
+        head=ptr;
+    }
     
-    printList(head);
+    void insertEnd(int x) {
+        ptr=head;
+        if(ptr==NULL) {                 // If list is empty
+            ptr=new node(x);
+            head=ptr;
+        }
+        else {
+            while(ptr->next!=NULL) {    // If list has not empty
+                ptr=ptr->next;
+            }
+            ptr->next=new node(x);
+        }
+    }
+
+    void deleteBegin() {
+        ptr=head;
+        if(ptr==NULL) {
+            cout<<"List is already empty\n";
+        }
+        else {
+            ptr=head->next;
+            delete head;
+            head=ptr;
+        }
+    }
+
+    void deleteEnd() {
+        ptr=head;
+        if(ptr==NULL) {
+            cout<<"List is already empty\n";
+        }
+        // Case for single node
+        else if(ptr->next==NULL) {
+            ptr=head->next;
+            delete head;
+            head=ptr;
+        }
+        // More than one node
+        else {
+            while(ptr->next->next!=NULL) {
+                ptr=ptr->next;
+            }
+            delete ptr->next;
+            ptr->next=NULL;
+        }
+    }
+
+    void insertAt(int pos, int x) {
+        node *newNode=new node(x);
+        ptr=head;
+        if(pos<1) {
+            cout<<"Position should be >= 1\n";
+        }
+        else if(pos==1) {
+            newNode->next=head;
+            head=newNode;
+        }
+        else {
+            for(int i=1;i<pos-1;i++) {
+                if(ptr!=NULL) {
+                    ptr=ptr->next;
+                }
+            }
+            if(ptr!=NULL) {
+                newNode->next=ptr->next;
+                ptr->next=newNode;
+            }
+            else {
+                cout<<"Previous node is NULL\n";
+            }
+
+        }
+    }
+
+};
+
+int main() {
+    LinkedList ll;
+
+    ll.insertEnd(10);
+    ll.printList();
+    ll.insertEnd(20);
+    ll.printList();
+    ll.insertEnd(30);
+    ll.printList();
+    ll.insertAt(6,15);
+    ll.printList();
     return 0;
 }
