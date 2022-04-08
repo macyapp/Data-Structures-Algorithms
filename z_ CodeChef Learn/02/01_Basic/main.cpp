e05426e492ccd143ac5b7328f1aff8775c07f6e7
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-04-08 21:34:11 +0530

    ud

---
+++ z_ CodeChef Learn/02/01_Basic/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    freopen("input.txt","r",stdin);
+    string name;
+    getline(cin,name);
+    cout<<"Hello, "<<name<<"\n";
+    return 0;
+}
