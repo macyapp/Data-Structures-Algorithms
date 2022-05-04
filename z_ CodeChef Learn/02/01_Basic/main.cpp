cea8c02ec540235d6a41b2e7c5ebc325fe5499d1
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-05-04 09:20:48 +0530

    ud

--- z_ CodeChef Learn/02/01_Basic/main.cpp
+++ z_ CodeChef Learn/02/01_Basic/main.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main() {
     freopen("input.txt","r",stdin);
-    
+    int t;
+    cin>>t;
+    while(t--) {
+        int t1,t2,s,n;
+        cin>>n>>s;
+        
+    }
     return 0;
 }
