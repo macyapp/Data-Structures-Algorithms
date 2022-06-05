6c2e567c925dd61a34718f947a51e0536b40b514
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-06-05 16:38:35 +0530

    ud

--- zDSA One/03_Mathematics/04_Recursion/main.cpp
+++ zDSA One/03_Mathematics/04_Recursion/main.cpp
@@ -2,9 +2,15 @@
 #include<iostream>
 using namespace std;
 
-
+int paths(int m, int n) {
+    if(m==1||n==1)
+        return 1;
+    return paths(m-1,n)+paths(m,n-1);
+}
 
 int main() {
-    
+    int m,n;
+    cin>>m>>n;
+    cout<<paths(m,n)<<"\n";
     return 0;
 }
