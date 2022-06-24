a0d2470da62e0ce4a943d55ac232c99e821721eb
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-06-24 10:51:35 +0530

    ud

--- zDSA One/06_Array/main.cpp
+++ zDSA One/06_Array/main.cpp
@@ -3,7 +3,27 @@
 #include<algorithm>
 using namespace std;
 
+void initialize(vector<int>& a,int n) {
+    int i,ip;
+    for(i=0;i<n;i++) {
+        a.push_back(ip);
+    }
+}
+
+void printArray(vector<int>& a,int x,int y) {
+    int i;
+    for(i=x;i<y;i++) {
+        cout<<a[i]<<" ";
+    }
+    cout<<"\n";
+}
+
 int main() {
-    cout<<"Hello, World\n";
+    freopen("ip.txt","r",stdin);
+    int n;
+    cin>>n;
+    vector<int> a;
+    initialize(a,n);
+    printArray(a,0,n);
     return 0;
 }
