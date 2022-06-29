6f4f5db83354ec15bc616797c2aec0a899420e23
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-06-29 16:54:48 +0530

    ud

--- zDSA One/06_Array/03_01_Stock Buy and Sell Brute Force.cpp
+++ zDSA One/06_Array/03_01_Stock Buy and Sell Brute Force.cpp
@@ -20,26 +20,18 @@ void print(vector<int> a,int x,int y) {
     cout<<"\n";
 }
 
-int calculate(vector<int>& a,int idx,int n) {
-    int i,j;
-    if(idx>=n)
-        return 0;
-    int maxProfit=0,profit,max;
-    for(i=idx;i<n;i++) {
-        max=0;
-        for(j=i+1;j<n;j++) {
-            if(a[j]>a[i]) {
-                profit=a[j]-a[i]+calculate(a,j+1,n);
-            }
-            max=(profit>max)?profit:max;
-        }
-        maxProfit=(max>maxProfit)?max:maxProfit;
-    }
-    return maxProfit;
-}
-
 void solve(vector<int>& a, int n) {
-    cout<<"Max Profit = "<<calculate(a,0,n)<<"\n";
+    int i,j;
+    int max=0,cur=0;
+    for(i=0;i<n-1;i++) {      // Buy
+        for(j=i+1;j<n;j++) {  // Sell
+            if(a[j]>a[i]) {
+                cur=a[j]-a[i];
+            }
+            max=(cur>max)?cur:max;
+        }
+    }
+    cout<<"Max profit = "<<max<<"\n";
 }
 
 int main() {
