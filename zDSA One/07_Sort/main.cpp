928269f26f5f9310042db424ea747d5b0a4d93b7
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-07-04 16:58:32 +0530

    ud

---
+++ zDSA One/07_Sort/main.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void initialize(vector<int>& a,int n) {
+    int i,input;
+    for(i=0;i<n;i++) {
+        cin>>input;
+        a.push_back(input);
+    }
+}
+
+void print(vector<int> a,int n) {
+    int i;
+    for(i=0;i<n;i++) {
+        cout<<a[i]<<" ";
+    }
+    cout<<"\n";
+}
+
+void merge(vector<int>& a,int start,int mid,int end) {
+    int i,j,p,q;
+    vector<int> temp(end-start+1,0);
+    j=0, p=start, q=mid+1;
+    for(i=0;i<=end;i++) {
+        if(p>mid) {                     // checks if first part comes to an end or not    
+            temp[j++]=a[q++];
+        }
+        else if(q>end) {                // checks if second part has come to an end or not
+            temp[j++]=a[p++];
+        }
+        else if(a[p]<a[q]) {            // checks which part has smaller element
+            temp[j++]=a[p++];
+        }
+        else {
+            temp[j++]=a[q++];
+        }
+    }
+    // Copying sorted temp array into actual array
+    for(i=0;i<j;i++) {
+        a[start++]=temp[i];
+    }
+}
+
+void mergeSort(vector<int>& a,int start,int end) {
+    int mid;
+    if(start<end) {
+        mid=(start+end)/2;          // defines the current array in 2 parts
+        mergeSort(a,start,mid);     // sort 1st part of array
+        mergeSort(a,mid+1,end);     // sort 2nd part of array
+        merge(a,start,mid,end);     // merge both parts by comparing elements of both parts
+    }
+}
+
+void solve(vector<int>& a, int n) {
+    mergeSort(a,0,n-1);
+}
+
+int main() {
+    freopen("ip.txt","r",stdin);
+    int n;
+    cin>>n;
+    vector<int> a;
+    initialize(a,n);
+    print(a,n);
+    solve(a,n);
+    print(a,n);
+    return 0;
+}
