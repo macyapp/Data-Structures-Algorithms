6cb2285fe9c14baeeff29a5679f6336b60f09b37
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-07-05 14:46:35 +0530

    ud

---
+++ zDSA One/07_Sort/main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void initialize(vector<int>& a,int n) {
+    int i,input;
+    for(i=0;i<n;i++) {
+        cin>>input;
+        a.push_back(input);
+    }
+}
+
+void print(vector<int> a,int n) {
+    int i;
+    for(i=0;i<n;i++) {
+        cout<<a[i]<<" ";
+    }
+    cout<<"\n";
+}
+
+void solve(vector<int>& a, int n) {
+    
+}
+
+int main() {
+    freopen("ip.txt","r",stdin);
+    int n;
+    cin>>n;
+    vector<int> a;
+    initialize(a,n);
+    print(a,n);
+    solve(a,n);
+    print(a,n);
+    return 0;
+}
--- z_ Playground/main.cpp
+++ z_ Playground/main.cpp
@@ -1,53 +1,29 @@
 #include<iostream>
-#include<vector>
 #include<algorithm>
+typedef long long ll;
 using namespace std;
 
-void initialize(vector<int>& a,int n) {
-    int i,input;
-    for(i=0;i<n;i++) {
-        cin>>input;
-        a.push_back(input);
+void solve(int n) {
+    ll b,c;
+    if(n&1) {
+        cout<<"-1\n";
     }
-}
-
-void print(vector<int> a,int x,int y) {
-    int i;
-    for(i=x;i<y;i++) {
-        cout<<a[i]<<" ";
-    }
-    cout<<"\n";
-}
-
-void solve(vector<int>& a, int n) {
-    int i;
-    int l=0,r=n-1;
-    int temp;
-    i=0;
-    while(i<=r) {
-        if(a[i]==0) {
-            swap(a[i],a[l]);
-            i++;
-            l++;
-        }
-        else if(a[i]==1) {
-            i++;
-        }
-        else if(a[i]==2) {
-            swap(a[i],a[r]);
-            r--;
-        }
+    else {
+        n>>=1;
+        b=1;
+        c= n^b;
+        cout<<b<<" "<<b<<" "<<c<<"\n";
     }
 }
 
 int main() {
     freopen("ip.txt","r",stdin);
-    int n;
-    cin>>n;
-    vector<int> a;
-    initialize(a,n);
-    print(a,0,n);
-    solve(a,n);
-    print(a,0,n);
+    int t;
+    cin>>t;
+    ll n;
+    while(t--) {
+        cin>>n;
+        solve(n);
+    }
     return 0;
 }
