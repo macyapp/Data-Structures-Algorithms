435e76cdd0ec9a948b9dc180007f818f10cd0fe3
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-07-24 17:04:41 +0530

    ud

--- z_ Playground/main.cpp
+++ z_ Playground/main.cpp
@@ -1,32 +1,20 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-void solve(int x,int y,int n,int r) {
-    int a,b,a_max,b_max;
-    bool flag=true;
-    for(b=0,a=n;b<=n;b++,a--) {
-        if(a*x+b*y==r) {
-            a_max=a;
-            b_max=b;
-        }
-    }
-    if(a_max>=0 && a_max<=n && b_max>=0 && b_max<=n) {
-        cout<<a_max<<" "<<b_max<<"\n";
-    }
-    else {
-        cout<<"-1\n";
-    }
+void solve(int a,int b,int n) {
+    
 }
 
 int main() {
     #ifndef ONLINE_JUDGE
         freopen("ip.txt","r",stdin);
     #endif
-    int t,x,y,n,r;
+    int t;
     cin>>t;
     while(t--) {
-        cin>>x>>y>>n>>r;
-        solve(x,y,n,r);
+        cin>>a>>b>>n;
+        solve(a,b,n);
     }
     return 0;
 }
