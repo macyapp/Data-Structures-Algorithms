1d3578dd11985d11bc3bebdcbe2ec0305702039c
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-08-01 17:51:45 +0530

    ud

--- z_ Playground/main.cpp
+++ z_ Playground/main.cpp
@@ -3,20 +3,40 @@
 #include<string>
 using namespace std;
 #define endl "\n"
+#define pb push_back
+
+vector<string> hashTable[20];
+int hashTableSize=20;
+
+int hashFunc(string s) {
+    
+}
+
+void insert(string s) {
+    int idx=hashFunc(s);
+    hashTable[idx].pb(s);
+}
+
+void search(string s) {
+    int idx=hashFunc(s);
+    int i;
+    for(i=0;i<hashTable[idx].size();i++) {
+        if(hashTable[idx][i]==s) {
+            cout<<s<<" is found!"<<endl;
+            return;
+        }
+    }
+    cout<<s<<" is not found!"<<endl;
+}
 
 void solve() {
-    string a,b;
-    
+
 }
 
 int main() {
     #ifndef ONLINE_JUDGE
         freopen("ip.txt","r",stdin);
     #endif
-    int t;
-    cin>>t;
-    while(t--) {
-        solve();
-    }
+    solve();
     return 0;
 }
