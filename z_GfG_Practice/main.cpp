37a8221ba387d85801beae8a7bb9c81a811ea2f2
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-08-07 19:04:39 +0530

    ud

--- z_GfG_Practice/main.cpp
+++ z_GfG_Practice/main.cpp
@@ -1,8 +1,51 @@
-#include<iostream>
+// { Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
-#define endl "\n"
+
+
+ // } Driver Code Ends
+class Solution{
+  public:
+    //Function to fill the array elements into a hash table 
+    //using Quadratic Probing to handle collisions.
+    void QuadraticProbing(vector <int>&hash, int hashSize, int arr[], int N) {
+        //Your code here
+        
+    }
+
+
+};
+
+// { Driver Code Starts.
 
 int main() {
-    
-    return 0;
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int hashSize;
+	    cin>>hashSize;
+	    
+	    
+	    int  N;
+	    cin>>N;
+	    int arr[N];
+	    
+	    for(int i=0;i<N;i++)
+	    cin>>arr[i];
+	    
+	    vector<int> hash (hashSize,-1);
+	    Solution obj;
+	    obj.QuadraticProbing (hash, hashSize, arr, N);
+	    
+	    for(int i = 0;i < hashSize; i++)
+	    cout<<hash[i]<<" ";
+	    
+	    cout<<endl;
+	    
+	}
+	return 0;
 }
+
+
+  // } Driver Code Ends
