543c4250e809b39b13704d899888fcc395284645
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-08-10 07:20:16 +0530

    ud

---
+++ Hashing/main.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define endl "\n"
+
+class Hash {
+    int n;
+    vector<int> v;
+public:
+    Hash(int n) {
+        v=vector<int>(n);
+        this->n=n;
+    }
+    int hashFunc(int x) {
+        return x%n;
+    }
+    void insert(int x) {
+        int idx=hashFunc(x);
+        while(v[idx]!=0) {
+            idx=(idx+1)%n;
+        }
+        v[idx]=x;
+    }
+    void search(int x) {
+        int idx=hashFunc(x);
+        while(v[idx]!=x && v[idx]!=0) {
+            idx=(idx+1)%n;
+        }
+        if(v[idx]==x) {
+            cout<<x<<" is found!"<<endl;
+        }
+        else {
+            cout<<x<<" is not found!"<<endl;
+        }
+    }
+    
+    void displayHash() {
+        int i,j;
+        for(i=0;i<v.size();i++) {
+            cout<<i;
+            if(v[i]!=0) {
+                cout<<" -> "<<v[i]<<endl;
+            }
+        }
+    }
+};
+
+int main() {
+    freopen("ip.txt","r",stdin);
+    int i,tmp,n;
+    cin>>n;
+    Hash ob(n);
+    for(i=0;i<n;i++) {
+        cin>>tmp;
+        ob.insert(tmp);
+    }
+    ob.displayHash();
+
+    ob.search(12);
+    return 0;
+}
---
+++ Hashing/mp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define endl "\n"
+
+int main() {
+    vector<int> v(5);
+    int i;
+    for(i=0;i<5;i++) {
+        
+    }
+    return 0;
+}
