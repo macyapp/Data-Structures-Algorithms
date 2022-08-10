2ac79ad646d23260cfa46c37cc93b311fdc52e1e
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-08-10 16:50:03 +0530

    ud

---
+++ Hashing/03_Quadratic Probing.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define endl "\n"
+
+class Hash {
+    int n;
+    vector<int> v;
+public:
+    Hash(int n) {
+        v=vector<int>(n,-1);
+        this->n=n;
+    }
+    int hashFunc(int x) {
+        return x%n;
+    }
+    void insert(int x) {
+        int idx=hashFunc(x);
+        int h=1;
+        while(v[idx]!=-1) {
+            idx=(idx+h*h)%n;
+            h++;
+        }
+        v[idx]=x;
+    }
+    void search(int x) {
+        int idx=hashFunc(x);
+        int h=1;
+        while(v[idx]!=x && v[idx]!=-1) {
+            idx=(idx+h*h)%n;
+            h++;
+        }
+        if(v[idx]==x) {
+            cout<<x<<" is found!"<<endl;
+        }
+        else {
+            cout<<x<<" is not found!"<<endl;
+        }
+    }
+    
+    void displayHash() {
+        int i,j;
+        for(i=0;i<v.size();i++) {
+            cout<<i;
+            if(v[i]!=0) {
+                cout<<" -> "<<v[i]<<endl;
+            }
+        }
+    }
+};
+
+int main() {
+    freopen("ip.txt","r",stdin);
+    int i,tmp,n;
+    cin>>n;
+    Hash ob(n);
+    for(i=0;i<n;i++) {
+        cin>>tmp;
+        ob.insert(tmp);
+    }
+    ob.displayHash();
+
+    ob.search(12);
+    return 0;
+}
--- Hashing/main.cpp
+++ Hashing/main.cpp
@@ -1,65 +1,17 @@
 #include<iostream>
-#include<vector>
+#include<unordered_set>
 using namespace std;
 #define endl "\n"
+#define us unordered_set
 
-class Hash {
+void solve() {
     int n;
-    vector<int> v;
-public:
-    Hash(int n) {
-        v=vector<int>(n);
-        this->n=n;
-    }
-    int hashFunc(int x) {
-        return x%n;
-    }
-    void insert(int x) {
-        int idx=hashFunc(x);
-        int h=1;
-        while(v[idx]!=0) {
-            idx=(idx+h*h)%n;
-            h++;
-        }
-        v[idx]=x;
-    }
-    void search(int x) {
-        int idx=hashFunc(x);
-        int h=1;
-        while(v[idx]!=x && v[idx]!=0) {
-            idx=(idx+h*h)%n;
-            h++;
-        }
-        if(v[idx]==x) {
-            cout<<x<<" is found!"<<endl;
-        }
-        else {
-            cout<<x<<" is not found!"<<endl;
-        }
-    }
-    
-    void displayHash() {
-        int i,j;
-        for(i=0;i<v.size();i++) {
-            cout<<i;
-            if(v[i]!=0) {
-                cout<<" -> "<<v[i]<<endl;
-            }
-        }
-    }
-};
+}
 
 int main() {
-    freopen("ip.txt","r",stdin);
-    int i,tmp,n;
-    cin>>n;
-    Hash ob(n);
-    for(i=0;i<n;i++) {
-        cin>>tmp;
-        ob.insert(tmp);
-    }
-    ob.displayHash();
-
-    ob.search(12);
+    #ifndef ONLINE_JUDGE
+        freopen("ip.txt","r",stdin);
+    #endif
+    solve();
     return 0;
 }
