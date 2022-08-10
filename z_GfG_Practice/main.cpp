959cda65d7fa9986eca8263a909c16696bbcbd46
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-08-10 22:30:12 +0530

    ud

--- z_GfG_Practice/main.cpp
+++ z_GfG_Practice/main.cpp
@@ -8,11 +8,28 @@ class Solution{
     // Function to count number of Ones
     // arr: input array 
     // N: size of input array
-    int countOnes(int arr[], int N)
-    {
-        
-        // Your code here
-        
+    int countOnes(int a[], int n) {
+        int l,h,m;
+        l=0;
+        h=n-1;
+        while(l<=h) {
+            m=(l+h)/2;
+            if(a[m]<1) {
+                h=m-1;
+            }
+            else if(a[m]>1) {
+                l=m+1;
+            }
+            else {
+                if(m==n-1 || a[m+1]!=1) {
+                    return m+1;
+                }
+                else {
+                    l=m+1;
+                }
+            }
+        }
+        return -1;
     }
 };
 
@@ -20,6 +37,9 @@ class Solution{
 
 int main()
 {
+#ifndef ONLINE_JUDGE
+freopen("ip.txt","r",stdin);
+#endif
     int t;
     cin>>t;
     
