09e4f9c4edc304860e76f5c92f847039cb53998c
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-08-11 07:34:18 +0530

    ud

--- Hashing/main.cpp
+++ Hashing/main.cpp
@@ -1,19 +1,27 @@
 #include<iostream>
-#include<unordered_set>
+#include<string>
+#include<unordered_map>
 using namespace std;
 #define endl "\n"
-#define us unordered_set
+#define um unordered_map
+#define fp first
+#define sp second
 
 void solve() {
-    int n;
-    bool flag=true;
-    int tmp;
+    um<string,int> m;
+    m["gfg"]=20;
+    m["ide"]=30;
+    m.insert({"courses",15});
+    cout<<m.size()<<endl;
+    m.erase("ide");
+    m.erase(m.begin(),m.end());
+    cout<<m.size()<<endl;
 }
 
 int main() {
-    #ifndef ONLINE_JUDGE
-        freopen("ip.txt","r",stdin);
-    #endif
+    // #ifndef ONLINE_JUDGE
+    //     freopen("ip.txt","r",stdin);
+    // #endif
     solve();
     return 0;
 }
