c54568c0663769ae84a9832667e30bd7dcfd33a3
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-08-25 13:17:05 +0530

    ud

--- Linked List/01_Interview Cake/01_Traversal of Linked List Recursive.cpp
+++ Linked List/01_Interview Cake/01_Traversal of Linked List Recursive.cpp
@@ -24,9 +24,11 @@ void print(node* head) {
 
 int main() {
     node* a=new node(10);
-    node* b=new node(20);
-    node* c=new node(30);
+    node* b=new node(5);
+    node* c=new node(20);
+    node* d=new node(15);
     a->next=b;
     b->next=c;
+    c->next=d;
     print(a);
 }
--- Linked List/01_Interview Cake/main.cpp
+++ Linked List/01_Interview Cake/main.cpp
@@ -21,23 +21,6 @@ void print(node* head) {
     print(head->next);
 }
 
-void search(node* head,int x) {
-    int pos=0;
-    node* cur=head;
-    while(cur!=nullptr) {
-        if(cur->data==x) {
-            pos++;
-            cout<<x<<" is present at "<<pos<<endl;
-            return;
-        }
-        else {
-            pos++;
-            cur=cur->next;
-        }
-    }
-    cout<<"Element not present\n";
-}
-
 int main() {
     node* a=new node(10);
     node* b=new node(5);
@@ -47,5 +30,5 @@ int main() {
     b->next=c;
     c->next=d;
     print(a);
-    search(a,20);
+    print(a);
 }
