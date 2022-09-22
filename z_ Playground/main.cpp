bd7fff9cb3d2c3470647280a89b4530c336e2c52
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-09-22 09:29:13 +0530

    ud

--- z_ Playground/main.cpp
+++ z_ Playground/main.cpp
@@ -1,17 +1,7 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
-void print(int n) {
-    if(n<1) {
-        return;
-    }
-    print(n-1);
-    cout<<n<<" ";
-}
-
 int main() {
-    print(9);
-    cout<<endl;
-    return 0;   
+    cout<<"Hello, world!\n";
+    return 0;
 }
