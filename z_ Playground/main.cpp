0992b0c67f07eea22bd798ef4ce23961b94d28ab
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2022-09-28 00:00:16 +0530

    ud

--- z_ Playground/main.cpp
+++ z_ Playground/main.cpp
@@ -1,7 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main() {
-    cout<<"Hello, World!\n";
-    return 0;
+template <class T, class U>
+T smaller(T a, U b) {
+    return (a<b?a:b);
+}
+
+int main () {
+    int x=72;
+    double y=15.34;
+    cout << smaller(x, y) << endl;
 }
