31e8b7813d0ce82cbb8d3662edff4054df59ddd4
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2023-01-09 11:35:08 +0530

    ud

--- z_Playground/main.cpp
+++ z_Playground/main.cpp
@@ -1,16 +1,9 @@
 #include<iostream>
+#include<iostream>
 using namespace std;
 
-bool setBit(int n, int k) {
-    if((n>>(k-1))&1)
-        return true;
-    return false;
-}
-
 void solve() {
-    int n,k;
-    cin>>n>>k;
-    cout<<boolalpha<<setBit(n,k)<<endl;
+
 }
 
 int main() {
