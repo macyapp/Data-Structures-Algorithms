89b76be8c7cc403dd7fa977ece43f8180ad640e9
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2023-02-06 23:00:07 +0530

    ud

--- z_Playground/main.cpp
+++ z_Playground/main.cpp
@@ -2,10 +2,19 @@
 using namespace std;
 
 void solve() {
-    
+    long long h,x,y;
+    cin>>h>>x>>y;
+    if(x>y)
+        cout<<"1\n";
+    else
+        count<<"0\n";
 }
 
 int main() {
-    solve();
+    int t;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
     return 0;
 }
