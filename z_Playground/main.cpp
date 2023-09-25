9af9101a9cb8b4130c2994ee666e00b0e993e964
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2023-09-25 21:38:06 +0530

    ud

--- z_Playground/main.cpp
+++ z_Playground/main.cpp
@@ -1,19 +1,38 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
 using namespace std;
 
-int fun(int n, int sum) {
-    if (n<1)
-        return 0;
-    return fun(n-1,sum);
-    return fun(n-3,sum);
-    return sum+1;
-}
+int kadaneAlgorithm(int array[], int size) {
+  int currentSum = 0;
+  int maxSoFar = INT_MIN;
+  int allNegativeSum = array[0];
 
-void solve() {
-    cout<<fun(5,0)<<endl;
+  for (int i = 0; i < size; i++) {
+    currentSum += array[i];
+    if (currentSum < 0) {
+      currentSum = 0;
+    }
+    maxSoFar = max(currentSum, maxSoFar);
+    allNegativeSum = min(allNegativeSum, array[i]);
+  }
+
+  if (allNegativeSum == 0) {
+    return maxSoFar;
+  }
+  else {
+    return allNegativeSum;
+  }
 }
 
 int main() {
-    solve();
-    return 0;
+  int array[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+  int size = sizeof(array) / sizeof(array[0]);
+
+  int maxSum = kadaneAlgorithm(array, size);
+
+  cout << "The maximum sum of a contiguous subarray is: " << maxSum << endl;
+
+  return 0;
 }
