440ea857daa72f7697d75e8db28f386e05dc56b8
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2023-11-16 10:36:04 +0530

    ud

--- z_Playground/main.cpp
+++ z_Playground/main.cpp
@@ -1,27 +1,57 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-void print(int n) {
-    if(n>=1) {
-        cout<<n<<" ";
-        print(n-1);
+void initialize(vector<int>& a, int n) {
+    int i, input;
+    for (i = 0; i < n; i++) {
+        cin >> input;
+        a.push_back(input);
     }
 }
 
-void printLine(int n) {
-    if(n>=1) {
-        printLine(n-1);
-        print(n);
-        cout<<endl;
+void print(vector<int>& a, int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << "\n";
+}
+
+void bubbleSortCompare(vector<int>& a, int j, int n, bool& swapped) {
+    if (j < n) {
+        if (a[j] > a[j + 1]) {
+            swap(a[j], a[j + 1]);
+            swapped = true;
+        }
+        bubbleSortCompare(a, j + 1, n, swapped);
     }
 }
 
-void solve() {
-    int n=9;
-    printLine(n);
+void bubbleSortPass(vector<int>& a, int n, bool swapped) {
+    if (n > 1 && swapped) {
+        swapped = false;
+        bubbleSortCompare(a, 0, n - 1, swapped);
+        bubbleSortPass(a, n - 1, swapped);
+    }
+}
+
+void solve(vector<int>& a, int n) {
+    bool swapped;
+    bubbleSortPass(a, n, swapped);
 }
 
 int main() {
-    solve();
+    int n;
+    cin >> n;
+    vector<int> a;
+    initialize(a, n);
+    cout << "Original Array: ";
+    print(a, n);
+
+    solve(a, n);
+
+    cout << "Sorted Array: ";
+    print(a, n);
     return 0;
 }
