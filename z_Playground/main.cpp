406f80e988b0aeb11eaa0401a08b09d21441df73
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2023-11-27 18:27:25 +0530

    ud

--- z_Playground/main.cpp
+++ z_Playground/main.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 void solve() {
-    
+    int num;
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++) {
+        num=i;
+        for(int j=1; j<=i; j++) {
+            cout<<num<<" ";
+            num+=(n-j);
+        }
+        cout<<endl;
+    }
 }
 
 int main() {
