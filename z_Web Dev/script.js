7a7fc81826600cfca5b8338236192e3b0b31b1d6
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2023-12-15 12:21:13 +0530

    ud

--- z_Web Dev/script.js
+++ z_Web Dev/script.js
@@ -1,2 +1,5 @@
-let myNumber = 32;
-console.log("0xff = "+x);
+const date=new Date();
+console.log(date.toDateString());
+let msec=Date.parse(date);
+console.log(msec);
+console.log(new Date(msec).toDateString())
