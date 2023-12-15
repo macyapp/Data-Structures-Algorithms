4c6c246521cc8080da49073d107a834551ed864f
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2023-12-15 12:38:51 +0530

    ud

--- z_Playground/main.cpp
+++ z_Playground/main.cpp
@@ -1,41 +1,11 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
-void initialize(vector<int> &a,int n) {
-    int i;
-    for(i=0;i<n;i++) {
-        cin>>a[i];
-    }
-}
-
-void print(vector<int> &a) {
-    for(auto i: a) {
-        cout<<i<<" ";
-    }
-    cout<<endl;
-}
-
-void solve(vector<int> &a,int n) {
-    int i=0,cur=0;
-    int count=0;
-    while(i<n) {
-        while(i<n && a[i]==a[cur]) {
-            count++;
-            i++;
-        }
-        cout<<a[cur]<<" "<<count<<endl;
-        cur=i;
-        count=0;
-    }
+void solve() {
+    
 }
 
 int main() {
-    int n;
-    cin>>n;
-    vector<int> a(n);
-    initialize(a,n);
-    print(a);
-    solve(a,n);
+    solve();
     return 0;
 }
