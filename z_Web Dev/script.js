3a883b97c0d21e2794411f8979104cf56322f4a6
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2024-02-16 21:19:09 +0530

    Update

--- z_Web Dev/script.js
+++ z_Web Dev/script.js
@@ -1,41 +1,7 @@
-class Solution {
-    maxSubArrayLength(nums) {
-        let n = nums.length;
-        let curSubarrayMax = 0, maxSum = 0;
-        let maxLength = 0, curLength = 0;
-        let maxElement = nums[0];
-        let maxElementLength = 0;
+let menu = document.getElementById("menu");
+// create a new node
+let li = document.createElement("li");
+li.textContent = "Home";
+// replace the first list item
 
-        for(let i = 0; i < n; i++) {
-            curSubarrayMax += nums[i];
-            if(curSubarrayMax < 0) {
-                curSubarrayMax = 0;
-                curLength = 0;
-            } else {
-                curLength++;
-                if(curSubarrayMax > maxSum) {
-                    maxSum = curSubarrayMax;
-                    maxLength = curLength;
-                }
-            }
-
-            if(nums[i] >= maxElement) {
-                maxElement = nums[i];
-                maxElementLength = 1;
-            }
-        }
-
-        return maxSum === 0 ? [maxElement, maxElementLength] : [maxSum, maxLength];
-    }
-}
-
-// Test the function
-const solution = new Solution();
-const n = parseInt(prompt("Enter the number of elements in the array: "));
-const nums = [];
-for(let i = 0; i < n; i++) {
-    nums.push(parseInt(prompt("Enter an integer:")));
-}
-const result = solution.maxSubArrayLength(nums);
-console.log("Maximum subarray sum:", result[0]);
-console.log("Maximum subarray length:", result[1]);
+menu.replaceChild(li, menu.firstElementChild);
